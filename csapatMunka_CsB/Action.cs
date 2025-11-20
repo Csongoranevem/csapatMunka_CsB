e7d0c282a8037943be77ab219fda7cfece989bc4
Acsbeni <acsbenjamin@turr.hu>
2025-11-20 14:03:27 +0100

    Javítása

--- csapatMunka_CsB/Action.cs
+++ csapatMunka_CsB/Action.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace csapatMunka_CsB
 {
     public class Action : MovieGenre
     {
+        // Új action-specifikus mezők
+        
         public string MainHeroName { get; set; }
         public string MainVillainName { get; set; }
-        public int StuntIntensityLevel { get; set; }
+        public int StuntIntensityLevel { get; set; }  
         public Action(
             string movie_Name,
             DateTime release_Date,
@@ -23,6 +24,7 @@ namespace csapatMunka_CsB
             string theme,
             string tone,
             string targetAudience,
+           
             string mainHeroName,
             string mainVillainName,
             int stuntIntensityLevel
@@ -30,14 +32,13 @@ namespace csapatMunka_CsB
         )
             : base(movie_Name, release_Date, movie_Type, director, music_Composer, money_Spent, income, name, theme, tone, targetAudience)
         {
-            
+          
             MainHeroName = mainHeroName;
             MainVillainName = mainVillainName;
             StuntIntensityLevel = stuntIntensityLevel;
-          
+            
         }
     }
 }
-
 
 
